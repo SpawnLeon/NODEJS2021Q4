7d7b41939ae05f8128bccb4e8ddb04b4552c5606
Alexander Leonov <spawnleon@gmail.com>
2021-11-15 17:54:55 +0300

    fix: remove commander package

--- task-1-ciphering-cli-tool/bin/caesar-cipher.js
+++ task-1-ciphering-cli-tool/bin/caesar-cipher.js
@@ -1,28 +1,34 @@
 #!/usr/bin/env node
 
-import program from 'commander';
 import app from '../src/index.js';
 
-const configOptionDescription = `config for ciphers Config is a string with pattern {XY(-)}n, where:
-      X is a cipher mark:
-      C is for Caesar cipher (with shift 1)
-      A is for Atbash cipher
-      R is for ROT-8 cipher
-      Y is flag of encoding or decoding (mandatory for Caesar cipher and ROT-8 cipher and should not be passed Atbash cipher)
-      1 is for encoding
-      0 is for decoding`;
+const options = {};
+const args = process.argv.splice(2);
 
-program
-  .storeOptionsAsProperties()
-  .description('CLI tool that will encode and decode a text by Caesar/Atbash/ROT-8 cipher.')
-  .version('1.0.0')
-  .option('-i, --input <string>', 'a path to input file', '')
-  .option('-o, --output <string>', 'output: a path to output file', '')
-  .requiredOption('-c, --config <string>', configOptionDescription, '')
-  .parse(process.argv);
+let counter = 0;
+while (counter < args.length) {
+  const arg = args[counter];
 
+  const nextArg = args[counter + 1];
+  if (!Object.prototype.hasOwnProperty.call(options, arg)) {
+    if (typeof nextArg === 'undefined') {
+      options[arg] = '';
+      break;
+    } else if (arg.startsWith('-') && !nextArg.startsWith('-')) {
+      options[arg] = nextArg;
+      counter += 2;
+    } else {
+      counter += 1;
+    }
+  } else {
+    console.error(`Error: argument '${arg}' is duplicated`);
+    process.exit(-1);
+  }
+}
 const {
-  input, output, config,
-} = program.opts();
+  '-i': input = '',
+  '-o': output = '',
+  '-c': config = '',
 
+} = options;
 app(input, output, config);
