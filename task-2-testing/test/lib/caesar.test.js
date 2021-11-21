9872d7c05c79f7de4b215e4a4c2e3c3800e3cca0
Alexander Leonov <spawnleon@gmail.com>
2021-11-21 07:23:47 +0300

    feat: add jest env for eslint

--- task-2-testing/test/lib/caesar.test.js
+++ task-2-testing/test/lib/caesar.test.js
@@ -1,7 +1,11 @@
 import { expect } from '@jest/globals';
 import caesar from '../../lib/caesar.js';
 
-test('test message', () => {
-  const result = caesar('This is secret. Message about "_" symbol!', 1);
-  expect(result).toBe('Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!');
+describe('function a', () => {
+  it('should return expected value', () => {
+    const result = caesar('This is secret. Message about "_" symbol!', 1);
+    expect(result).toBe('Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!');
+  });
 });
+
+console.log(111);
