49a5237dc97416073ad1d54a0d7a4f5ec1a96712
Alexander Leonov <spawnleon@gmail.com>
2021-11-21 07:41:22 +0300

    feat: add testing caesar chipper

--- task-2-testing/test/lib/caesar.test.js
+++ task-2-testing/test/lib/caesar.test.js
@@ -1,11 +1,23 @@
-import { expect } from '@jest/globals';
 import caesar from '../../lib/caesar.js';
 
-describe('function a', () => {
-  it('should return expected value', () => {
+describe('Caesar chipper module', () => {
+  it('testing shift 1', () => {
     const result = caesar('This is secret. Message about "_" symbol!', 1);
     expect(result).toBe('Uijt jt tfdsfu. Nfttbhf bcpvu "_" tzncpm!');
   });
-});
 
-console.log(111);
+  it('testing shift -1', () => {
+    const result = caesar('This is secret. Message about "_" symbol!', -1);
+    expect(result).toBe('Sghr hr rdbqds. Ldrrzfd zants "_" rxlank!');
+  });
+
+  it('cyrillic symbols do not change, shift 10', () => {
+    const result = caesar('Кириллические символы', 10);
+    expect(result).toBe('Кириллические символы');
+  });
+
+  it('numbers do not change, shift 2', () => {
+    const result = caesar('1234567890', 2);
+    expect(result).toBe('1234567890');
+  });
+});
